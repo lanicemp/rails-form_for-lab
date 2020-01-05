d405700d27d4e63b0c94c5926724edd0aee6df28
LaNice Powell <lanicemp@aol.com>
2020-01-05 18:37:02 -0500

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,14 @@
 class SchoolClassesController < ApplicationController
+
+    def new 
+        @school_class = SchoolClass.new
+    end 
+    
+    def create 
+        @school_class = SchoolClass.new 
+        @school_class.title = params[:title]
+        @school_class.room_number = params[:room_number]
+        @school_class.save 
+        redirect_to school_class_path(@school_class)
+    end   
 end
