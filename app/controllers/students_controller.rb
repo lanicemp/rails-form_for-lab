53b890e72d2ff04fb9682e454ed1c9a936b8caba
LaNice Powell <lanicemp@aol.com>
2020-01-05 18:00:01 -0500

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,29 @@
+class StudentsController < ApplicationController
+
+    def index 
+        @students = Student.all 
+    end 
+
+    def show 
+        @students = Student.find(params[:id])
+    end 
+    
+    def new 
+        @student = Student.new 
+    end 
+
+    def create 
+        @student = Student.new 
+        @student.first_name = params[:first_name]
+        @student.last_name = params[:last_name]
+        @student.save 
+        redirect_to student_path(@student)
+    end 
+    
+    def edit 
+    end 
+
+    def update 
+    end 
+
+end
